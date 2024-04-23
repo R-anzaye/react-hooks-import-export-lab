e02d39cad2f0c18c424c5362a18edb73bf7abf6b
rileydollar8@gmail.com <ryan.anzaye@student.moringaschool.com>
2024-04-23 16:35:25 +0300

    import-export

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <div>
